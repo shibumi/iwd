87da7a5ca0e25dfcbb22968e5bb9cca226469b5f
Owen Rouge <owen.rouge@atos.net>
2021-06-14 17:33:10 +0200

    test5

--- device.go
+++ device.go
@@ -4,6 +4,7 @@ import "github.com/godbus/dbus/v5"
 
 const (
 	objectDevice = "net.connman.iwd.Device"
+	object       = "net.connman.iwd"
 )
 
 // Device refers to the iwd network device like "wlan0" for example: /net/connman/iwd/0/4
@@ -17,7 +18,7 @@ type Device struct {
 }
 
 func (d Device) SetAp(conn *dbus.Conn) error {
-	obj := conn.Object(objectDevice, d.Path)
+	obj := conn.Object(object, d.Path)
 	err := obj.SetProperty(objectDevice+".Mode", dbus.MakeVariant("ap"))
 	if err != nil {
 		return err
